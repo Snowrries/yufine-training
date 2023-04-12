f620cac6e32a93c961cdc1cd58bbcc0b285c4322
Allen Tung <Snowrries@users.noreply.github.com>
2023-04-12 21:29:06 +0000

    Update add endpoint to accept N params

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,7 +7,18 @@ def hello_world():
     return "<p>Hello, World! This is Yufine Training Co.</p>"
 
 
-@app.route("/add/")
-def add_2_and_2():
-    return f"<p>2 + 2 = {2+2}</p>"
+@app.route("/add/", methods=['GET', 'POST'])
+def add():
+    data = request.json
+    
+    output = ""
+    sum = 0
+    for a in data[0:len(data)-1]:
+        output += f"{a} + "
+        sum += a
 
+    output += f"{data[-1]}"
+    sum += data[-1]
+    output += f" = {sum}"
+    
+    return output
